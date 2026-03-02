48b51a94ff034fe36901c87186bc1727f29ea490
Saurabh Singh <saurabh500@users.noreply.github.com>
2026-03-02 09:09:07 +0000

    Sort module declarations alphabetically (cargo fmt)

--- src/lib.rs
+++ src/lib.rs
@@ -9,8 +9,8 @@ pub mod sql_int16;
 pub mod sql_int32;
 pub mod sql_int64;
 pub mod sql_money;
-pub mod sql_string;
 pub mod sql_single;
+pub mod sql_string;
 
 pub use error::SqlTypeError;
 pub use sql_boolean::SqlBoolean;
@@ -23,5 +23,5 @@ pub use sql_int16::SqlInt16;
 pub use sql_int32::SqlInt32;
 pub use sql_int64::SqlInt64;
 pub use sql_money::SqlMoney;
-pub use sql_string::SqlString;
 pub use sql_single::SqlSingle;
+pub use sql_string::SqlString;
